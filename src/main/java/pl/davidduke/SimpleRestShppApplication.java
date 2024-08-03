e3f5cec81690a535772e7ac50caf3ad91b65d4ee
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-08-03 14:19:48 +0200

    First commit

---
+++ src/main/java/pl/davidduke/SimpleRestShppApplication.java
@@ -0,0 +1,13 @@
+package pl.davidduke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestShppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestShppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/davidduke/SimpleRestShppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.davidduke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestShppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
