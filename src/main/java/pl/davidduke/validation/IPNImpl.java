ec978934dd867d84870f55bc57a75bb659ae7fe9
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-08-10 23:02:01 +0200

    Changed IPN validator logic

--- src/main/java/pl/davidduke/validation/IPNImpl.java
+++ src/main/java/pl/davidduke/validation/IPNImpl.java
@@ -7,7 +7,7 @@ import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class IPNImpl implements ConstraintValidator<IPN, String> {
-    final int[] PALINOM = new int[] {-1, 5, 7, 9, 4, 6, 10, 5, 7};
+    static final int[] COEFFICIENTS = new int[] {-1, 5, 7, 9, 4, 6, 10, 5, 7};
     @Override
     public void initialize(IPN constraintAnnotation) {
         // nothing to initialize
@@ -15,12 +15,12 @@ public class IPNImpl implements ConstraintValidator<IPN, String> {
 
     @Override
     public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        if (s.isBlank() || !s.matches("^[0-9]*$")) {
+        if (s.isBlank() || !s.matches("\\d{10}")) {
             return false;
         }
         int controlSum = 0;
-        for (int i = 0; i < PALINOM.length; i++) {
-            controlSum += PALINOM[i] * Integer.parseInt(String.valueOf(s.charAt(i)));
+        for (int i = 0; i < COEFFICIENTS.length; i++) {
+            controlSum += COEFFICIENTS[i] * Integer.parseInt(String.valueOf(s.charAt(i)));
         }
         int controlNumber = controlSum % 11 % 10;
         return s.endsWith(String.valueOf(controlNumber));
