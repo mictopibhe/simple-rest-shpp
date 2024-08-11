086e86e567bb264c0d4cf941e0d491132f9d3489
Aleksander Davydiuk <mictopibhe@gmail.com>
2024-08-11 09:51:52 +0200

    Added validator tests implementation

---
+++ src/test/java/pl/davidduke/validation/IPNImplTest.java
@@ -0,0 +1,33 @@
+package pl.davidduke.validation;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+class IPNImplTest {
+
+    IPNImpl validator = new IPNImpl();
+
+    @Test
+    void isValidShouldReturnTrueWhenIPNIsValid() {
+        String ipn = "2248000331";
+        assertTrue(validator.isValid(ipn, null));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "2248000332",
+            "1",
+            "avdkgyatsd",
+            "11111111111",
+    })
+    void isValidShouldReturnFalseWhenIPNIsInvalid(String value) {
+        assertFalse(validator.isValid(value, null));
+    }
+}
